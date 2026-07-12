289c9f7fbf22d14fc3476c2b53ce4b0b2906834b
Soban Mehboob <soban.mehboob@econceptions.mobi>
2026-07-12 13:43:24 +0500

    grafana port added

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,6 +48,12 @@ resource "aws_security_group" "notepad_mern_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+  from_port   = 3000
+  to_port     = 3000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
   egress {
     from_port   = 0
